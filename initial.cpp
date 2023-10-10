5d5a12adec34607798db94d14f8860305b3a136d
BhupinderJ <sbhupinderj9721@gmail.com>
2023-10-11 00:36:30 +0530

    initial files

---
+++ initial.cpp
@@ -0,0 +1,12 @@
+// ~BhupinderJ
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+const int mod = 1e9+7;
+const int nmx = 1e6+1;
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cout << "Apple\n";
+}
